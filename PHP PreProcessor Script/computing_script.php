8207f679edfa0077ccb4689ed688cfc137e4be33
Kim Daniel <64163182+KIMdaniiell@users.noreply.github.com>
2021-12-08 23:27:42 +0100

    Add files via upload

--- PHP PreProcessor Script/computing_script.php
+++ PHP PreProcessor Script/computing_script.php
@@ -1,105 +1,105 @@
 <?php
-    function isHit($x,$y,$r) {
-        if (isRightTop($x,$y,$r)||isRightBottom($x,$y,$r)||isLeftTop($x,$y,$r)){
-            return true;
-        }
-        return false;
-    }
+function isHit($x, $y, $r)
+{
+    return (isRightTop($x, $y, $r) || isRightBottom($x, $y, $r) || isLeftTop($x, $y, $r));
+}
 
-    function isRightTop($x,$y,$r) {
-        if (($x>=0)&&($y>=0)&&($x**2 +   $y**2 <= $r**2)){
-            return true;
-        }
-        return false;
-    }
+function isRightTop($x, $y, $r)
+{
+    return ($x >= 0) && ($y >= 0) && ($x ** 2 + $y ** 2 <= $r ** 2);
+}
 
-    function isRightBottom($x,$y,$r) {
-        if (($x>=0)&&($y<=0)&&($x-$y<=$r/2)){
-            return true;
-        }
-        return false;
-    }
+function isRightBottom($x, $y, $r)
+{
+    return ($x >= 0) && ($y <= 0) && ($x - $y <= $r / 2);
+}
 
-    function isLeftTop($x,$y,$r) {
-        if (($x<=0)&&($y>=0)&&($x>=-$r)&&($y<=$r)){
-            return true;
-        }
-        return false;
-    }
+function isLeftTop($x, $y, $r)
+{
+    return ($x <= 0) && ($y >= 0) && ($x >= -$r) && ($y <= $r);
+}
 
-    function xIsValid($x){
-        /** Выполняет валидацию поля X.
-         * x ∈ { -4, -3, -2, -1, 0, 1, 2, 3, 4}
-        **/
+function xIsValid($x)
+{
+    /** Выполняет валидацию поля X.
+     * x ∈ { -4, -3, -2, -1, 0, 1, 2, 3, 4}
+     **/
 
-        //  preg_match("/^-?[0-4]&/", $x)
-        return ($x == "-4" || $x == "-3" || $x == "-2" || $x == "-1" || $x == "0" || $x == "1" || $x == "2" || $x == "3" || $x == "4");
-    }
-    function yIsValid($y){
-        /** Выполняет валидацию поля Y.
-         * y ∈ { -3, ..., 5}
-         **/
+    //  preg_match("/^-?[0-4]&/", $x)
+    return ($x == "-4" || $x == "-3" || $x == "-2" || $x == "-1" || $x == "0" || $x == "1" || $x == "2" || $x == "3" || $x == "4");
+}
 
-        $reg1 = "/^-3(\.0+)?$/";        //  [ -3.(0) ; -3 ]
-        $reg2 = "/^-?[0-2](\.\d+)?$/";  //  ( -3 ; 3 )
-        $reg3 = "/^[34](\.\d+)?$/";     //  [ 3 ; 5 )
-        $reg4 = "/^5(\.0+)?$/";         //  [ 5 ; 5.(0) )
-        return (preg_match($reg1,$y)
-            ||preg_match($reg2,$y)
-            ||preg_match($reg3,$y)
-            ||preg_match($reg4,$y));
-    }
-    function rIsValid($r){
-        /** Выполняет валидацию поля R.
-         * x ∈ { 1, 1.5, 2, 2.5, 3}
-         **/
+function yIsValid($y)
+{
+    /** Выполняет валидацию поля Y.
+     * y ∈ { -3, ..., 5}
+     **/
 
-        //preg_match("/^-?[1-3]$/", $r)
-        //preg_match("/^-?[1-2]\.5$/", $r)
-        return ($r == "1" || $r == "1.5" || $r == "2" || $r == "2.5" || $r == "3");
-    }
+    $reg1 = "/^-3(\.0+)?$/";        //  [ -3.(0) ; -3 ]
+    $reg2 = "/^-?[0-2](\.\d+)?$/";  //  ( -3 ; 3 )
+    $reg3 = "/^[34](\.\d+)?$/";     //  [ 3 ; 5 )
+    $reg4 = "/^5(\.0+)?$/";         //  [ 5 ; 5.(0) )
+    return (preg_match($reg1, $y)
+        || preg_match($reg2, $y)
+        || preg_match($reg3, $y)
+        || preg_match($reg4, $y));
+}
 
+function rIsValid($r)
+{
+    /** Выполняет валидацию поля R.
+     * x ∈ { 1, 1.5, 2, 2.5, 3}
+     **/
 
-    date_default_timezone_set('Europe/Moscow');
-    /**global $x;
-    global $y;
-    global $r;
+    //preg_match("/^-?[1-3]$/", $r)
+    //preg_match("/^-?[1-2]\.5$/", $r)
+    return ($r == "1" || $r == "1.5" || $r == "2" || $r == "2.5" || $r == "3");
+}
+$x = "-";
+$y = "-";
+$r = "-";
+$result = "ОШИБКА POST ЗАПРОСА";
+$current_time = "-";
+$processing_time = "-";
+
+//====================================================================================================
+
+if (!empty($_POST))  {
+
+    //Проверка на установленность всех значений
     if (isset($_POST['X']) && isset($_POST['Y']) && isset($_POST['R'])) {
         $x = trim($_POST['X']);
         $y = trim($_POST['Y']);
         $r = trim($_POST['R']);
-    }*/
-        $x = $_POST['X'];
-        $y = $_POST['Y'];
-        $r = $_POST['R'];
 
-    $result = "Ошибка ввода";
-    if (xIsValid($x)&&yIsValid($y)&&rIsValid($r)) {
-        if (isHit($x, $y, $r)) {
-            $result = "Попадание";
-        } else {
-            $result = "Промах";
+        //Проверка на валидность всех значений
+        if (xIsValid($x)&&yIsValid($y)&&rIsValid($r)) {
+            //Проверка на попадание
+            if (isHit($x, $y, $r)) {
+                $result = "Попадание";
+            } else {
+                $result = "Промах";
+            }
+
+            date_default_timezone_set('Europe/Moscow');
+            $processing_time = round((microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]), 6);
+            $current_time = date('G:i:s', time());
+
+        }else {
+            $result = "Некорректные данные!";
         }
+    } else {
+        $result = "Не все поля заполнены!";
     }
-
-    $processing_time = round((microtime(true) - $_SERVER["REQUEST_TIME_FLOAT"]), 6);
-    $current_time = date('G:i:s', time());
-
-//ОКРУЛИ ДО какого нибудь знака!
-//ОКРУЛИ ДО какого нибудь знака!
-//ОКРУЛИ ДО какого нибудь знака!
-//ОКРУЛИ ДО какого нибудь знака!
-//ОКРУЛИ ДО какого нибудь знака!
-//ОКРУЛИ ДО какого нибудь знака!
+}
 
 $response = '{
-"x": "'.$x.'",
-"y": "'.$y.'",
-"r": "'.$r.'",
-"result": "'.$result.'",
-"ctime": "'.$current_time.'",
-"etime": '.$processing_time.'
-}';
+                "x": "' . $x . '",
+                "y": "' . $y . '",
+                "r": "' . $r . '",
+                "result": "' . $result . '",
+                "ctime": "' . $current_time . '",
+                "etime": "' . $processing_time . '"
+                }';
 echo $response;
 
-?>
